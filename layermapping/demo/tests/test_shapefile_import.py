43c4d79875475924e3f49b50fe7e17f34c4208c7
Kathryn Killebrew <kkillebrew@azavea.com>
2018-12-12 11:47:34 -0500

    Add failing test for recognizing nulls
    Add failing test to demonstrate LayerMapping imports nulls as zeroes.
    
    See: https://code.djangoproject.com/ticket/30020#ticket.

--- layermapping/demo/tests/test_shapefile_import.py
+++ layermapping/demo/tests/test_shapefile_import.py
@@ -13,9 +13,20 @@ TESTDATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
 
 class ImportShapefileTestCase(TestCase):
 
-    def test_import_shapefile_command(self):
+    def setUp(self):
+        """Call management command to import the test shapefile."""
         shapefile = os.path.join(TESTDATA_PATH, 'demo_shapefile', 'demo.shp')
         call_command('import_shapefile', shapefile)
+
+    def test_shapefile_command_imported_objects(self):
         self.assertEqual(3, DemoGeoModel.objects.all().count())
         self.assertEqual(1, DemoGeoModel.objects.filter(name='Philadelphia').count())
-        self.assertEqual(1, DemoGeoModel.objects.filter(some_number__gt=0).count())
+
+    def test_shapefile_command_recognized_nulls(self):
+        # Should have three objects: one has 0, another a truthy value, and the third is null
+        self.assertEqual(1, DemoGeoModel.objects.filter(some_number__isnull=True).count(),
+                         'Should have one DemoGeoModel with a null some_number')
+        self.assertEqual(1, DemoGeoModel.objects.filter(some_number__gt=0).count(),
+                         'Should have one DemoGeoModel with a some_number value > 0')
+        self.assertEqual(1, DemoGeoModel.objects.filter(some_number=0).count(),
+                         'Should have one DemoGeoModel with a zero some_number')
