2a59d7ec8d84e915fa9779e338a988b67f3e69bc
Kathryn Killebrew <kkillebrew@azavea.com>
2018-12-12 17:47:54 -0500

    Run GIS tests on Django core
    Add script to run core Django suite GIS tests on PostGIS in containers.

---
+++ layermapping/test_postgis.py
@@ -0,0 +1,23 @@
+# This is a test settings file for use with the Django test suite.
+#
+# https://docs.djangoproject.com/en/dev/internals/contributing/writing-code/unit-tests/
+#
+# The different databases that Django supports behave differently in certain
+# situations, so it is recommended to run the test suite against as many
+# database backends as possible.  You may want to create a separate settings
+# file for each of the backends you test against.
+
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.getenv('POSTGRES_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT')
+    },
+}
+
+SECRET_KEY = os.getenv('DJANGO_SECRET')
